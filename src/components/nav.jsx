95ef7fb70d5d8b967a698379148c326747995c25
Alex Hiller <asonnyhiller@gmail.com>
2023-08-14 15:44:35 -0400

    created nav bar

---
+++ src/components/nav.jsx
@@ -0,0 +1,41 @@
+import { NavLink } from 'react-router-dom';
+
+const nav = () => {
+
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <NavLink>
+                    Top
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink>
+                    About Me
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink>
+                    Skills
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink>
+                    Projects
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink>
+                    Socials
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink>
+                    Contact Me!
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
+    )
+}
