edc0d5a75ffe96e74c6e7339514dc1a0719c591a
Alex Hiller <asonnyhiller@gmail.com>
2023-08-20 16:15:34 -0400

    finished up with the basic skeleton for each component

--- src/components/feedback.jsx
+++ src/components/feedback.jsx
@@ -1,5 +1,21 @@
 const feedback = () => {
 
+    return (
+        <>
+            <form onSubmit = {null}>
+                <label>
+                    Email:
+                    <input type="text" name="name" />
+                </label>
+                <label>
+                    Comment:
+                    <textarea name="comment" rows="5" cols="33" />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        </>
+    )
+
 }
 
 export default feedback;
--- src/components/socials.jsx
+++ src/components/socials.jsx
@@ -1,4 +1,22 @@
+import { Link } from 'react-router-dom';
+
 const socials = () => {
+
+    return (
+        <>
+            <ul>
+                <li>
+                    <a href="https://www.linkedin.com/in/alex-hiller-422baa163/">LinkedIn</a>
+                </li>
+                <li>
+                    <a href="https://wellfound.com/u/alex-hiller">Wellfound</a>
+                </li>
+                <li>
+                    <a href="https://github.com/Dudemaster47">GitHub</a>
+                </li>
+            </ul>
+        </>
+    )
 
 }
 
