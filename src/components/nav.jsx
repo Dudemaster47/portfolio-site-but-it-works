fd701a668cf331fccdda8dfece7adba7f99abcd2
Alex Hiller <asonnyhiller@gmail.com>
2023-09-09 16:06:21 -0400

    added class names to nav bar

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -4,35 +4,35 @@ import { NavLink } from 'react-router-dom';
 function Nav() {
 
     return (
-        <nav>
-            <ul>
-                <li>
-                    <NavLink to='/' exact={true}>
+        <nav className='outerbar'>
+            <ul className="navbar">
+                <li className="navbutton">
+                    <NavLink to='/' exact={true} activeClassName='active'>
                     Top
                     </NavLink>
                 </li>
-                <li>
-                    <NavLink to='/aboutme' exact={true}>
+                <li className="navbutton">
+                    <NavLink to='/aboutme' exact={true} activeClassName='active'>
                     About Me
                     </NavLink>
                 </li>
-                <li>
-                    <NavLink to='/skills' exact={true}>
+                <li className="navbutton">
+                    <NavLink to='/skills' exact={true} activeClassName='active'>
                     Skills
                     </NavLink>
                 </li>
-                <li>
-                    <NavLink to='/projects' exact={true}>
+                <li className="navbutton">
+                    <NavLink to='/projects' exact={true} activeClassName='active'> 
                     Projects
                     </NavLink>
                 </li>
-                <li>
-                    <NavLink to='/socials' exact={true}>
+                <li className="navbutton">
+                    <NavLink to='/socials' exact={true} activeClassName='active'>
                     Socials
                     </NavLink>
                 </li>
-                <li>
-                    <NavLink to='/feedback' exact={true}>
+                <li className="navbutton">
+                    <NavLink to='/feedback' exact={true} activeClassName='active'>
                     Contact Me!
                     </NavLink>
                 </li>
