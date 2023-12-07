cc9cb6f995208b828acc3f20094330c5afa002da
Alex Hiller <asonnyhiller@gmail.com>
2023-12-07 15:51:48 -0500

    ok break time

--- src/components/project_links.jsx
+++ src/components/project_links.jsx
@@ -34,7 +34,7 @@ function ProjectLinks() {
         // </div>
         <>
             <div className="projectBox">
-                <h3 className="projectHeader" id="P1">Homecooked</h3>
+                <h2 className="projectHeader" id="P1">Homecooked</h2>
                 <div className="infobox" id="I1">
                     <div className="textbox">
                         <p>A fusion of Doordash and Etsy, it's a website where you can set up your own shop and sell homecooked food directly to the masses!
@@ -42,8 +42,10 @@ function ProjectLinks() {
                             A user can order food from an existing store, create and customize their own store, review products, view the averaged score of a store, and even check out with a fully functional cart.
                             There's also a useful search function to search for exactly what you want to purchase. The logo was designed by yours truly, and the site was a group project with Megha Sahgal and Daniel Yoo.
                         </p>
-                        <Link to="https://home-cooked.onrender.com/">Website</Link>
-                        <Link to="https://github.com/kympanic/homecooked/">Github Link</Link>
+                        <div className="linkBox">
+                            <Link to="https://home-cooked.onrender.com/">Website</Link>
+                            <Link to="https://github.com/kympanic/homecooked/">Github Link</Link>
+                        </div>
                     </div>
                     <div className="picturebox">
                         <div className="pbRow1">
@@ -57,15 +59,17 @@ function ProjectLinks() {
                     </div>
 
                 </div>
-                <h3 className="projectHeader" id="P2">Card Games!!</h3>
+                <h2 className="projectHeader" id="P2">Card Games!!</h2>
                 <div className="infobox" id="I2">
                     <div className="textbox">
                         <p>A fairly basic website where you can set up an account and play War against a CPU opponent. It was developed using a Flask back end and a React-Redux front end.
                             Currently, you can create a profile, add custom deck images to use for yourself, and create a game to play. The whole site isn't fully functional currently, but it does feature fully functioning gameplay.
                             I'll admit this is a bit rough appearance-wise and lacks in style, but the main focus is the game itself which works perfectly. Someday I'll return to it and add further functionality.
                         </p>
-                        <Link to="https://card-games.onrender.com/">Website</Link>
-                        <Link to="https://github.com/Dudemaster47/Card-Game-Capstone">Github Link</Link>
+                        <div className="linkBox">                            
+                            <Link to="https://card-games.onrender.com/">Website</Link>
+                            <Link to="https://github.com/Dudemaster47/Card-Game-Capstone">Github Link</Link>
+                        </div>
                     </div>
                     <div className="picturebox">
                         <div className="pbRow1">
@@ -78,14 +82,16 @@ function ProjectLinks() {
                         </div>
                     </div>
                 </div>
-                <h3 className="projectHeader" id="P3">Blackjack.JS</h3>
+                <h2 className="projectHeader" id="P3">Blackjack.JS</h2>
                 <div className="infobox" id="I3">
                     <div className="textbox">
                         <p>A simple application that allows you to play blackjack in a VS Code terminal utilizing Node JS. Built entirely in javascript with a class-based architecture.
                             There's not a huge amount to say. It allows for single player and local multiplayer, and has a betting system and keeps track of "money" that is won and lost.
                             There's no dedicated website for this game, but it can be downloaded through github. To do: add a readme that explains how to play...
                         </p>
-                        <Link to="https://github.com/Dudemaster47/blackjack2">Github Link</Link>
+                        <div className="linkBox">   
+                            <Link to="https://github.com/Dudemaster47/blackjack2">Github Link</Link>
+                        </div>
                     </div>
                     <div className="picturebox">
                         <div className="pbRow1">
