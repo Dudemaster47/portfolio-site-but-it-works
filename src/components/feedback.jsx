4c30c6e81d80f6c54c3747e8f8f50e0cd8447ead
Alex Hiller <asonnyhiller@gmail.com>
2024-02-08 04:08:09 -0500

    contact me page is nicer looking

--- src/components/feedback.jsx
+++ src/components/feedback.jsx
@@ -1,5 +1,5 @@
 import { useRef } from "react";
-import emailjs from '@emailjs/browser';
+import emailjs from "@emailjs/browser";
 import ashLogo from "../images/ash-logo.png";
 
 export const Feedback = () => {
@@ -8,12 +8,21 @@ export const Feedback = () => {
     const sendEmail = (e) => {
         e.preventDefault();
 
-        emailjs.sendForm('contact_service', 'template_a7efzwo',form.current,'upzXRB1xcGlkey4qB')
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            });
+        emailjs
+            .sendForm(
+                "contact_service",
+                "template_a7efzwo",
+                form.current,
+                "upzXRB1xcGlkey4qB"
+            )
+            .then(
+                (result) => {
+                    console.log(result.text);
+                },
+                (error) => {
+                    console.log(error.text);
+                }
+            );
     };
 
     return (
@@ -23,34 +32,50 @@ export const Feedback = () => {
                     <img className="miniLogo" src={ashLogo} alt="logo" />
                 </div>
                 <div className="splashtext3">
-                    <h1>Contact Me</h1> 
+                    <h1>Contact Me</h1>
                 </div>
             </div>
-            <div className="splashbox">
-                <form
-                    ref={form}
-                    onSubmit={sendEmail}
-                    className="commentForm"
-                >
-                    <label htmlFor="name">Name: </label>
-                    <input
-                        type="text"
-                        name="user_name"
-                    />
-                    <label htmlFor="email">Email: </label>
-                    <input
-                        type="email"
-                        name="user_email"
-                    />
-                    <br />
-                    <label htmlFor="message">Message:</label>
-                    <textarea
-                        name="message"
-                        rows="5"
-                        cols="33"
-                    />
-                    <input type="submit" value="Send" className="submitButton"/>
-                </form>
+            <div className="feedbackBox">
+                <div className="splashbox formBox">
+                    <p className="feedbackShpiel">
+                        Thanks for visiting my portfolio page! I hope to expand
+                        it further in the future- if you're interested in
+                        getting in contact with me, just leave your name, email
+                        address, and a message here and I'll respond as soon as
+                        possible.
+                    </p>
+                    <form
+                        ref={form}
+                        onSubmit={sendEmail}
+                        className="commentForm"
+                    >
+                        <label htmlFor="name">Name: </label>
+                        <input
+                            className="textBox"
+                            type="text"
+                            name="user_name"
+                        />
+                        <label htmlFor="email">Email: </label>
+                        <input
+                            className="textBox"
+                            type="email"
+                            name="user_email"
+                        />
+                        <br />
+                        <label htmlFor="message">Message:</label>
+                        <textarea
+                            className="textBox bigBox"
+                            name="message"
+                            rows="5"
+                            cols="33"
+                        />
+                        <input
+                            type="submit"
+                            value="Send"
+                            className="submitButton"
+                        />
+                    </form>
+                </div>
             </div>
         </>
     );
